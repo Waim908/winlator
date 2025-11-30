b04ef6730d374a90ebed3e17fb3750c092c882f1
Waim908 <waimxj449@foxmail.com>
2025-11-30 18:59:54 +0800

    update turnip (dev)

---
+++ .github/workflows/turnip-winlator.sh
@@ -0,0 +1,58 @@
+export CFLAGS="-O3 -march=armv8-a -mtune=generic -flto=auto -s"
+export CXXFLAGS="-O3 -march=armv8-a -mtune=generic -flto=auto -s"
+export LDFLAGS="-O3 -flto=auto -s"
+
+export LD_RPATH=/data/data/com.winlator/files/rootfs/usr/lib
+export LD_FILE=$LD_RPATH/ld-linux-aarch64.so.1
+
+mkdir -p /data/data/com.winlator/files/rootfs/usr/
+pip install mako --break-system-package
+pip install PyYAML --break-system-package
+pip install meson --break-system-package
+cd /tmp
+git clone https://github.com/brunodev85/mesa3d-custom
+cd mesa3d-custom
+cd $1 || exit 1
+
+# 如果这个环境连dri都不支持就换成xlib
+meson setup builddir \
+	--strip \
+	-Dprefix=/data/data/com.winlator/files/rootfs/usr/ \
+	-Dbuildtype=release \
+	-Dplatforms=x11 \
+	-Dgallium-drivers='' \
+	-Dvulkan-drivers=freedreno \
+	-Degl=disabled \
+	-Dgles1=disabled \
+	-Dgles2=disabled \
+	-Dglvnd=enabled \
+	-Dglx=dri \
+	-Dllvm=disabled \
+	-Dgallium-extra-hud=true \
+	-Dgallium-vdpau=disabled \
+	-Dgbm=enabled \
+	-Dvulkan-layers=device-select,overlay \
+	-Dfreedreno-kmds=kgsl \
+	-Dlibunwind=disabled \
+	-Dvalgrind=disabled \
+	-Dmicrosoft-clc=disabled \
+	-Dvideo-codecs=all || exit 1
+
+meson compile -C builddir || exit 1
+meson install -C builddir
+
+cd /data/data/com.winlator/files/rootfs/usr/lib
+patchelf --set-rpath "$LD_RPATH" --set-interpreter "$LD_FILE" libvulkan_freedreno.so
+
+ls
+
+mkdir -p /tmp/output/usr/lib
+mkdir -p /tmp/output/usr/share/vulkan/icd.d/
+
+cp -r -p libvulkan_freedreno.so /tmp/output/usr/lib
+cd /data/data/com.winlator/files/rootfs/usr/share/vulkan/icd.d/
+sed -i 's/"api_version": "[^"]*"/"api_version": "1.1.246"/' freedreno_icd.aarch64.json || exit 1
+cp -r -p freedreno_icd.aarch64.json /tmp/output/usr/share/vulkan/icd.d/
+
+cd /tmp/output/
+tar -I 'zstd -T8 -9' -cvf /tmp/turnip-$2.tzst . || exit 1
